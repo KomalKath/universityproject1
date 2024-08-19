061777b9536b2db9630a575390a3ade66960e3ce
KomalKath <149062619+KomalKath@users.noreply.github.com>
2024-08-19 12:50:47 +0500

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,14 +4,14 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     //console.log(process.env.MONGODB_URL);
     try {
-        await mongoose.connect('mongodb+srv://komalkatherine:komal4215@crowndb.9qyqgdq.mongodb.net/?retryWrites=true&w=majority&appName=crowndb', {
+        await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Error connecting to MongoDB', error);
-        process.exit(1);
+        // process.exit(1);
     }
 };
 
