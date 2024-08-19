cce52339c3fe3cebce95bdd437e3453ef8de2d03
KomalKath <149062619+KomalKath@users.noreply.github.com>
2024-08-19 14:55:13 +0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const serverless = require('serverless-http');
+const Student = require('../models/student');
 const app = express();
 
 // Middleware to parse JSON bodies
